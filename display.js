a0c0695d63237ee04c8547727e0360ecd4527681
maurolps <boroengenharia@gmail.com>
2023-10-04 16:51:26 -0300

    Added UpdateWeather function

--- display.js
+++ display.js
@@ -1,7 +1,29 @@
 import {forecast} from './script.js';
 
 function updateWeatherData (weatherData) {
+  const cityName = document.querySelector('.js-city-name')
+  const location = document.querySelector('.js-location');
+  const date = document.querySelector('.js-date');
+  const ctemperature = document.querySelector('.js-temperature');
+  const condition = document.querySelector('.js-condition');
+  const feelslike = document.querySelector('.js-feelslike');
+  const wind = document.querySelector('.js-wind');
+  const humidity = document.querySelector('.js-humidity');
+  const icon = document.querySelector('.js-city-icon');
   console.log('update: ', weatherData);
+
+  const temperature = weatherData.current.temp_c.toFixed(0);
+  ctemperature.textContent = `${temperature}`;
+
+  cityName.textContent = `${weatherData.location.name}`
+  location.textContent = `${weatherData.location.region}, ${weatherData.location.country}`;
+  date.textContent = `${weatherData.location.localtime}`;
+  condition.textContent = `${weatherData.current.condition.text}`;
+  feelslike.textContent = `Feels like: ${weatherData.current.feelslike_c}`;
+  wind.textContent = `Wind: ${weatherData.current.wind_kph}`;
+  humidity.textContent = `Humidity: ${weatherData.current.humidity} %`;
+
+  icon.setAttribute('src', weatherData.current.condition.icon);
 }
 
 export function eventHandler () {
@@ -11,8 +33,10 @@ export function eventHandler () {
   btnSearch.addEventListener('click', () => {
     const city = inputSearch.value;
     if (city !== '') {
-      console.log ('search: ', city);
-      const weatherData = forecast(city);
+      //remove invalid characters
+      const cityClean = city.normalize('NFD').replace(/\p{Mn}/gu, "");
+      console.log ('search: ', cityClean);
+      const weatherData = forecast(cityClean);
       weatherData.then((data) =>
         {
           updateWeatherData(data)
--- script.js
+++ script.js
@@ -5,16 +5,6 @@ async function forecast (city) {
   try {
   const response = await fetch(`${weatherApi}${city}`, {"mode": "cors"});
   const weatherData = await response.json();
-  console.log('Country: ', weatherData.location.country);
-  console.log('Localtime: ', weatherData.location.localtime);
-  console.log('name: ', weatherData.location.name);
-  console.log('Region: ', weatherData.location.region);
-  console.log('');
-  console.log('Condition: ', weatherData.current.condition.text);
-  console.log('Feels Like C: ', weatherData.current.feelslike_c);
-  console.log('Feels Like F: ', weatherData.current.feelslike_f);
-  console.log('Wind Mph: ', weatherData.current.wind_mph); // 1 Mph = 0,44704 m/s
-  console.log('Humidity: ', weatherData.current.humidity, '%');
   return weatherData;
 
   } catch (err) {
@@ -23,11 +13,6 @@ async function forecast (city) {
 }
 
 eventHandler();
-
-// temp function to clear console
-setTimeout(() => {
-console.clear();  
-}, 3000);
 
 export {forecast};
 
