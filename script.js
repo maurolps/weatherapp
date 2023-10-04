59e0b961d5240df4ee3be2ffa206e58fa8a39486
maurolps <boroengenharia@gmail.com>
2023-10-04 19:06:56 -0300

    Forecast function loads next days forecast

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 import {eventHandler} from './display.js';
 
-async function forecast (city) {
-  const weatherApi = 'https://api.weatherapi.com/v1/current.json?key=2460c90164ff4b66a89171433230310&q=';
-  try {
-  const response = await fetch(`${weatherApi}${city}`, {"mode": "cors"});
-  const weatherData = await response.json();
-  return weatherData;
+function formatDate (date) {
+  // yyyy-MM-dd for weather history api
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
 
-  } catch (err) {
-    console.error(err);
+  return `${year}-${month}-${day}`;
+}
+
+
+async function forecast (city) {
+  const weatherFuture = 'https://api.weatherapi.com/v1/forecast.json?key=2460c90164ff4b66a89171433230310';
+  const today = new Date();
+  today.setDate(today.getDate() + 1);
+  const dt = formatDate(today);
+  try {
+    const response = await fetch(`${weatherFuture}&days=3&q=${city}`, {"mode": "cors"});
+    const weatherFutureData = await response.json();
+    return weatherFutureData;
+    } catch (err) {
+      console.error(err); 
   }
 }
 
-eventHandler();
-
 export {forecast};
 
+eventHandler();
